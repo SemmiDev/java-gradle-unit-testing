4f9fddbc064ed6cc86a974a8f098b4fbbd480b06
SemmiDev <sammidev4@gmail.com>
2020-10-03 19:29:31 +0700

    eksekusi parallel

--- src/test/java/com/sammidev/SlowTest.java
+++ src/test/java/com/sammidev/SlowTest.java
@@ -2,14 +2,25 @@ package com.sammidev;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import java.util.concurrent.TimeUnit;
 
-class incSlowTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@Execution(value = ExecutionMode.CONCURRENT)
+class SlowTest {
 
     @Test
     @Timeout(value = 5, unit = TimeUnit.SECONDS)
     void show() throws InterruptedException {
         Thread.sleep(10_000);
+    }
+
+    @Test
+    void show2() {
+        assertEquals(1+1,2);
     }
 }
